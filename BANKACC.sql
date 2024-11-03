9086c11ed60bd7c4075dbb07275361a048a6a256
Maksimmetla <125728591+Maksimmetla@users.noreply.github.com>
2024-11-03 14:15:02 +0300

    Delete BANKACC.sql

--- BANKACC.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE transfer(
-transferID SERIAL PRIMARY KEY,
-transfers_to_usersID INT,
-from_card_to_cardID INT,
-transfer_amount DOUBLE PRECISION,
-FOREIGN KEY (transferID) REFERENCES history_of_operations(transferID),
-FOREIGN KEY (transfers_to_usersID) REFERENCES transfers_to_users(userID),
-FOREIGN KEY (from_card_to_cardID) REFERENCES from_card_to_card(cardID)
-);
